bc4c2458639e958a6c72d07ad1942f54a27d654c
joelrodriguesvieira <joel2j39@gmail.com>
2023-11-16 15:07:29 -0300

    Add Javascript

---
+++ src/scripts/products.js
@@ -0,0 +1,57 @@
+export const products = [
+    {
+        id: 0,
+        titulo: "Milk Shake",
+        img: "../src/imgs/milk-shakes.png",
+        preco: "11,99"
+    },
+    {
+        id: 1,
+        titulo: "Club Mix",
+        img: "../src/imgs/club-mix.png",
+        preco: "9,99"
+    },
+    {
+        id: 2,
+        titulo: "Açai",
+        img: "../src/imgs/acai.png",
+        preco: "11,99"
+    },
+    {
+        id: 3,
+        titulo: "Linha Kids",
+        img: "../src/imgs/linha-kids.png",
+        preco: "19,99"
+    },
+    {
+        id: 4,
+
+        titulo: "Mini Sorvete",
+        img: "../src/imgs/mini.png",
+        preco: "6,99"
+    },
+    {
+        id: 5,
+        titulo: "Picolé",
+        img: "../src/imgs/picole.png",
+        preco: "4,99"
+    },
+    {
+        id: 6,
+        titulo: "Sobremesa da Casa",
+        img: "../src/imgs/sobremesas.png",
+        preco: "13,99"
+    },
+    {
+        id: 7,
+        titulo: "Sundae",
+        img: "../src/imgs/sundae.png",
+        preco: "13,99"
+    },
+    {
+        id: 8,
+        titulo: "Maxxi Gelato",
+        img: "../src/imgs/maxxi-gelato.png",
+        preco: "11,99"
+    },
+];
